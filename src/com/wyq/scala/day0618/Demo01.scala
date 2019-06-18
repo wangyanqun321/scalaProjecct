33ed551670662eade8dfad3a0d1e642e7e74f9bc
wangyanqun2017 <1264359108@qq.com>
2019-06-18 23:31:12 +0800

    2019/6/18

---
+++ src/com/wyq/scala/day0618/Demo01.scala
@@ -0,0 +1,11 @@
+package com.wyq.scala.day0618
+
+/**
+  * 一个方法如何没有参数则可以不写（）
+  */
+object Demo01 {
+  def main(args: Array[String]): Unit = {
+    val num1: Int = 10
+    println(num1.toDouble + num1.toString)
+  }
+}
